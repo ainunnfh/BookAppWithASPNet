19bf13df883110e10313480e7a6a889f1f17225f
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-24 11:47:40 +0700

    ⭐ connect db and create db context

---
+++ BookApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Books> Books { get; set; }
+    }
+}
