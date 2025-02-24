4a51e427a97b99d2121b301d25e238eb2eb96a99
Ainun Nafiah <ainunnafiah354@gmail.com>
2025-02-24 10:36:04 +0700

    Add project files.

---
+++ BookApp/Models/Books.cs
@@ -0,0 +1,10 @@
+﻿namespace BookApp.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public required string book_title { get; set; } = string.Empty;
+        public required string book_desc { get; set; } = string.Empty;
+        public required string book_author { get; set; } = string.Empty;
+    }
+}
